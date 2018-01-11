d4e627aa85367d8116d5e196a24c5ad561d9a174
Potluri <potlurs@amazon.com>
2018-01-11 12:20:56 -0800

    Updated UploadToS3 Mojo based on new UploadToS3Input model

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 To install: `mvn install`
 <br />
 To run the foo MOJO: `mvn com.amazonaws:aws-maven-toolkit:0.0.1:foo -Dfoo.projectName=myProject`
+<br /> 
+To upload files to S3 `mvn com.amazonaws:aws-maven-toolkit:0.0.1:uploadToS3 -Ds3-upload.bucketName=<bucket name> -Ds3-upload.keyName=<key_name> -Ds3-upload.source=<absolute file path> -Daws-credentials.profileName=default -Ds3-upload.region=us-east-1`
--- src/main/java/com/amazonaws/toolkit/maven/mojos/UploadBuildArtifactsToS3Mojo.java
+++ src/main/java/com/amazonaws/toolkit/maven/mojos/UploadBuildArtifactsToS3Mojo.java
@@ -10,14 +10,23 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import com.amazonaws.toolkit.core.hackathon.ActionContext;
-import com.amazonaws.toolkit.core.hackathon.actions.UploadArtifactsToS3Action;
+import com.amazonaws.toolkit.core.hackathon.actions.UploadToS3Action;
 import com.amazonaws.toolkit.core.hackathon.analytics.NoOpEventPublisher;
-import com.amazonaws.toolkit.core.hackathon.models.UploadArtifactsToS3Input;
+import com.amazonaws.toolkit.core.hackathon.models.AwsScope;
+import com.amazonaws.toolkit.core.hackathon.models.UploadToS3Input;
+import com.amazonaws.toolkit.core.hackathon.models.UploadToS3Input.EncryptionType;
 import com.amazonaws.toolkit.maven.core.NullToolkitProgressor;
 import com.amazonaws.toolkit.maven.core.SimpleLogger;
 
-@Mojo(name = "s3UploadArtifacts", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+@Mojo(name = "uploadToS3", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
 public class UploadBuildArtifactsToS3Mojo extends AbstractMojo {
+    // TODO: Encryption
+
+    @Parameter(property = "aws-credentials.profileName", required = true)
+    private String profileName;
+
+    @Parameter(property = "s3-upload.region", required = true)
+    private String region;
 
     @Parameter(property = "s3-upload.bucketName", required = true)
     private String bucketName;
@@ -28,14 +37,15 @@ public class UploadBuildArtifactsToS3Mojo extends AbstractMojo {
     @Parameter(property = "s3-upload.keyName", required = true)
     private String keyName;
 
+    // Not currently used.
     @Parameter(property = "s3-upload.createBucketIfDoesntExist", defaultValue = "false")
     private boolean createBucketIfDoesntExist;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         ActionContext context = new ActionContext(new NoOpEventPublisher().createEvent("Foo"),
                 new SimpleLogger(), new NullToolkitProgressor());
-        UploadArtifactsToS3Input input = new UploadArtifactsToS3Input(bucketName, source, keyName, createBucketIfDoesntExist);
-        new UploadArtifactsToS3Action().execute(input, context);
+        AwsScope scope = new AwsScope(profileName, region);
+        UploadToS3Input input = new UploadToS3Input(scope, source.getAbsolutePath(), bucketName, keyName, EncryptionType.NONE, null);
+        new UploadToS3Action().execute(input, context);
     }
-
 }
